4bc0d4d9e69236268a3b5ecec78f89c39947fd53
xum35 <moucheng.xu2@medtronic.com>
2026-02-15 01:23:16 +0000

    adding new bash exp for full fine tuning

--- exps/train.sh
+++ exps/train.sh
@@ -12,7 +12,8 @@ NPROC_PER_NODE=$(echo "${CUDA_VISIBLE_DEVICES}" | awk -F',' '{print NF}')
 OMP_NUM_THREADS=$((256 / NPROC_PER_NODE))
 
 # Set up the experiment name defined in the config file (double check the config file to ensure it matches, i had this mistakes too many times):
-EXP=predict2_lora_training_2b_laparoscopic_txt
+# EXP=predict2_lora_training_2b_laparoscopic_txt_lora
+EXP=predict2_video2world_training_2b_cosmos_laparoscopic
 
 # Run the training script with torchrun
 torchrun --nproc_per_node=${NPROC_PER_NODE} -m scripts.train \
